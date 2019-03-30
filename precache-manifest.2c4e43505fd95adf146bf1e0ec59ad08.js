196762f3c15e3fd56ae2b1538f9b83a4fc10e276
Oliu Radu <oliuradu@gmail.com>
2019-03-30 20:06:19 +0100

    Test

---
+++ precache-manifest.2c4e43505fd95adf146bf1e0ec59ad08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "faefd01eff8dafa97a26",
+    "url": "/static/css/main.55b821d4.chunk.css"
+  },
+  {
+    "revision": "faefd01eff8dafa97a26",
+    "url": "/static/js/main.93d85b69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b5d56b6b69088e2e0558",
+    "url": "/static/css/2.d3e07a03.chunk.css"
+  },
+  {
+    "revision": "b5d56b6b69088e2e0558",
+    "url": "/static/js/2.fe03593f.chunk.js"
+  },
+  {
+    "revision": "140a7c82fd8eae581cca58bf437ae984",
+    "url": "/static/media/logo.140a7c82.svg"
+  },
+  {
+    "revision": "2abc4b1cbf52377bdb03ea274971ebe0",
+    "url": "/index.html"
+  }
+];
